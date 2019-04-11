fabf5f0bfbfed8e00ecd92edeeb2225ceaa8a5a2
unknown <1050238242@qq.com>
2019-04-11 15:46:05 +0800

    p1.py

---
+++ p2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 21:21:05 2019
+
+@author: 10502
+"""
+class Polynomial:
+    def __init__(self,dic):
+        self.dic=dic
+    def __call__(self,x):
+        c=0
+        for k in self.dic:
+            c+=self.dic[k]*x**(k)
+        return c
+    def __add__(self,another):
+        result={}
+        for k in self.dic:
+            result[k]=self.dic[k]
+            for j in another.dic:
+                if k==j:
+                    result[k]=self.dic[k]+another.dic[j]
+                else:
+                    result[j]=another.dic[j]
+        return Polynomial(result)
+
+
